08d0980e8db5b2938a189d9fee408e005d4716e5
FoxxMD <matt.duncan13@gmail.com>
2015-11-06 17:17:23 -0500

    Improve address validation
    * use postal code validtor
    * relax name restrictions

--- src/Models/Request/ToAddress.php
+++ src/Models/Request/ToAddress.php
@@ -34,11 +34,11 @@ class ToAddress extends BaseToAddress implements Validatable
     {
         return [
             v::attribute('email', v::oneOf(v::nullValue(), v::email())),
-            v::attribute('name', v::notEmpty()->alpha()),
+            v::attribute('name', v::notEmpty()->string()),
             v::attribute('street1', v::string()->notEmpty()),
             v::attribute('city', v::alpha()->notEmpty()),
             v::attribute('stateProvince', v::alpha()->notEmpty()),
-            v::attribute('postalCode', v::int()->notEmpty()),
+            v::attribute('postalCode', v::postalCode($this->country)->notEmpty()),
             v::attribute('country', v::countryCode()->notEmpty()),
             v::attribute('phone', v::notEmpty()->phone())
         ];
